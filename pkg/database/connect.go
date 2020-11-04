1ba39b2ef655d6a5b4f6400b07dc41d19f071c29
江口 雄也 <egcyuya@yahoo.co.jp>
2020-11-05 06:16:48 +0900

    con

--- pkg/database/connect.go
+++ pkg/database/connect.go
@@ -19,11 +19,7 @@ func Connect() *sql.DB {
 		//panic(err.Error())
 	}
 
-	registerTlsConfig("../rds-ca-2019-root.pem", "custom")
-	connectionstring := os.Getenv("DATABASE_URL")
-	if connectionstring == "" {
-		connectionstring = os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@" + os.Getenv("DB_HOST") + "/" + os.Getenv("DB_NAME") + ""
-	}
+	connectionstring := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@" + os.Getenv("DB_HOST") + "/" + os.Getenv("DB_NAME") + ""
 	db, err := sql.Open("mysql", connectionstring)
 	if err != nil {
 		panic(err.Error())
